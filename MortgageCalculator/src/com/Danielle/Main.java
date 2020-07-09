63d8d1a6ed73f0c992019f9848c3105545c9c60b
dnjx <djohnson5196@gmail.com>
2020-07-09 15:24:15 -0700

    Adjust formatting for UJP Module 17

--- MortgageCalculator/src/com/Danielle/Main.java
+++ MortgageCalculator/src/com/Danielle/Main.java
@@ -1,5 +1,6 @@
 package com.Danielle;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 
@@ -8,6 +9,9 @@ public class Main {
 
     public static void main(String[] args) {
 	//principal annual interest rate period
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your principal amount: ");
         int principal = scanner.nextInt();
@@ -16,20 +20,21 @@ public class Main {
         System.out.print("Enter your period (in years): ");
         Byte loan_period = scanner.nextByte();
 
-        Float annual_interest_rate = interest_amount /100;
-        Float monthly_interest_rate = annual_interest_rate / 12;
+        Float annualInterestRate = interest_amount / PERCENT;
+        Float monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR;
 
-        int num_payments = loan_period * 12;
+        int num_payments = loan_period * MONTHS_IN_YEAR;
 
-        Double numerator = monthly_interest_rate * (Math.pow(1 + monthly_interest_rate, num_payments));
-        Double denominator = (Math.pow(1 + monthly_interest_rate, num_payments)) - 1;
+        Double numerator = monthlyInterestRate * (Math.pow(1 + monthlyInterestRate, num_payments));
+        Double denominator = (Math.pow(1 + monthlyInterestRate, num_payments)) - 1;
         Double monthly_payment = principal * (numerator/denominator);
+        String monthly_payment_formatted = NumberFormat.getCurrencyInstance().format(monthly_payment);
 
 
         System.out.println("Your principal amount is: " + principal);
-        System.out.println("Your annual interest rate is: " + annual_interest_rate);
+        System.out.println("Your annual interest rate is: " + annualInterestRate);
         System.out.println("Your load period is: " + loan_period + " years");
-        System.out.println("Your monthly payment is: "  + monthly_payment);
+        System.out.println("Your monthly payment is: "  + monthly_payment_formatted);
 
     }
 }
