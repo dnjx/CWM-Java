0418ed6798ebdcb18dfe1c72fda11f7998f73a34
dnjx <d.johnson.5196@gmail.com>
2020-07-10 18:58:50 -0700

    Add control some input validation to calculator

--- MortgageCalculator/src/com/Danielle/Main.java
+++ MortgageCalculator/src/com/Danielle/Main.java
@@ -3,37 +3,59 @@ package com.Danielle;
 import java.text.NumberFormat;
 import java.util.Scanner;
 
-
-
 public class Main {
 
     public static void main(String[] args) {
-	//principal annual interest rate period
+        //principal annual interest rate period
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
+        final int MIN_PRINCIPAL = 1000;
+        final int MAX_PRINCIPAL = 1_000_000;
 
+        int principal = 0;
+        double interestAmount = 0.0;
+        byte loanPeriod = 0;
+        double annualInterestRate = 0.0;
+        double monthlyInterestRate = 0.0;
+        int numPayments = 0;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your principal amount: ");
-        int principal = scanner.nextInt();
-        System.out.print("Enter your annual interest rate amount: ");
-        Float interest_amount = scanner.nextFloat();
-        System.out.print("Enter your period (in years): ");
-        Byte loan_period = scanner.nextByte();
 
-        Float annualInterestRate = interest_amount / PERCENT;
-        Float monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR;
 
-        int num_payments = loan_period * MONTHS_IN_YEAR;
+        while (true) {
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
+            if (principal >= MIN_PRINCIPAL && principal <= MAX_PRINCIPAL)
+                break;
+            System.out.println("Invalid amount. Please enter a principal amount between $1k and $1M.");
+        }
+        while (true) {
+            System.out.print("Annual interest rate: ");
+            interestAmount = scanner.nextDouble();
+            if (interestAmount > 0.0 && interestAmount <= 30) {
+                annualInterestRate = interestAmount / PERCENT;
+                monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Invalid amount. Please enter an interest rate between 0.1% and 30%.");
+        }
+        while (true) {
+            System.out.print("Loan period:  ");
+            loanPeriod = scanner.nextByte();
+            if(loanPeriod >= 1 && loanPeriod <= 30) {
+                numPayments = loanPeriod * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Invalid period. Please enter a loan period between 1 & 30 years.");
+        }
 
-        Double numerator = monthlyInterestRate * (Math.pow(1 + monthlyInterestRate, num_payments));
-        Double denominator = (Math.pow(1 + monthlyInterestRate, num_payments)) - 1;
-        Double monthly_payment = principal * (numerator/denominator);
+        double numerator = monthlyInterestRate * (Math.pow(1 + monthlyInterestRate, numPayments));
+        double denominator = (Math.pow(1 + monthlyInterestRate, numPayments)) - 1;
+        double monthly_payment = principal * (numerator/denominator);
+
         String monthly_payment_formatted = NumberFormat.getCurrencyInstance().format(monthly_payment);
-
-
         System.out.println("Your principal amount is: " + principal);
         System.out.println("Your annual interest rate is: " + annualInterestRate);
-        System.out.println("Your load period is: " + loan_period + " years");
+        System.out.println("Your load period is: " + loanPeriod + " years");
         System.out.println("Your monthly payment is: "  + monthly_payment_formatted);
 
     }
